37af5367b57a9adf132949f04d048218593a5ef0
Matt <director@acidgambit.com>
2024-02-06 09:48:23 -0500

    d2 part 2

--- 2022/python/day2.py
+++ 2022/python/day2.py
@@ -3,11 +3,36 @@
 # scoring: paper: 1, rock: 2, scissors: 3
 # + 3 for draw, 6 for win, 0 for loss
 
+# part 2:
+# X = lose, Y = draw, Z = win
+
+# ex
+# opp chooses Rock, needs to end in Y, or a draw. If Y, match opponent A = X
+# if ends X = loss, pick what opponent beats (player chooses B paper, pick X rock)
+# if ends Z = win, pick what Z wins => player chooses C scissors, pick X paper
+
 choices = {'X': 1, 'Y': 2, 'Z': 3}
 outcomes = {'win': 6, 'draw': 3, 'loss': 0}
 match = {'X': 'A', 'Y': 'B', 'Z': 'C'}
 
 beats = {'Y': 'A', 'Z': 'B', 'X': 'C'}
+
+# Part 2
+opp_wins = {'A': 'Z', 'B': 'X', 'C': 'Y'}
+opp_loses = {'A': 'Y', 'B': 'Z', 'C': 'X'}
+opp_draw = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+
+
+def calc_choice(opponent, outcome):
+    if outcome == 'Y': # Draw
+        player_choice = opp_draw.get(opponent)
+        return player_choice
+    elif outcome == 'X': # Lose
+        player_choice = opp_wins.get(opponent)
+        return opp_wins.get(opponent)
+    elif outcome == 'Z': # Win
+        player_choice = opp_loses.get(opponent)
+        return player_choice
 
 def calc_winner(opponent, player):
     if match.get(player) == opponent:
@@ -17,6 +42,7 @@ def calc_winner(opponent, player):
     else:
         return 'loss'
     
+# Part 1
 def game(file):
     total = 0
     with open(file, 'r') as f:
@@ -32,7 +58,22 @@ def game(file):
             total += game
     return total
 
+# Part 2
+def game2(file):
+    total = 0
+    with open(file, 'r') as f:
+        for l in f:
+            opponent_choice, outcome = l.strip().split()
+            player_choice = calc_choice(opponent_choice, outcome)
+            choice_score = choices[player_choice]
+            result = calc_winner(opponent_choice, player_choice)
+            result_score = outcomes[result]
+            game = choice_score + result_score
+            total += game
+    return total
 
 file = '../inputs/d2.txt'
-total_score = game(file)
-print(f"Total Score: {total_score}")
+game1_total_score = game(file)
+game2_total_score = game2(file)
+print(f"Game 1 Total Score: {game1_total_score}")
+print(f"Game 2 Total Score: {game2_total_score}")
